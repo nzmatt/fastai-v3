10a9cf5f9472d2ac6d437f91d46ad9e098c36bbe
nzmatt <31487273+nzmatt@users.noreply.github.com>
2019-02-08 14:16:26 +1100

    Update server.py

--- app/server.py
+++ app/server.py
@@ -8,7 +8,7 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-export_file_url = 'https://www.dropbox.com/s/iqm96ga0p41yxk2/renoClassifier.pkl?dl=1'
+export_file_url = 'https://www.dropbox.com/s/7xndjstjsdwd8h5/renoClassifier.pkl?dl=1'
 export_file_name = 'renoClassifier.pkl'
 
 classes = ['new', 'old']
